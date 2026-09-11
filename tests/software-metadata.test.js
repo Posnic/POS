33ab13afe06b5e741c9f9f15aac548cd1e78780d
Sridhar Bala <sridharkalaibala@gmail.com>
2026-09-11 06:29:08 +0530

    fix: replace obsolete Posnic root domain

--- tests/software-metadata.test.js
+++ tests/software-metadata.test.js
@@ -42,7 +42,7 @@ test('CodeMeta identifies point-of-sale scope without promoting development as r
   assert.deepEqual(metadata.runtimePlatform, ['Electron', 'Node.js', 'MongoDB']);
   assert.equal(metadata.isSourceCodeOf?.['@type'], 'SoftwareApplication');
   assert.equal(metadata.isSourceCodeOf?.name, 'Posnic POS');
-  assert.equal(metadata.isSourceCodeOf?.url, 'https://posnic.io/');
+  assert.equal(metadata.isSourceCodeOf?.url, 'https://www.posnic.com/');
   assert.match(metadata.developmentStatus, /active development/i);
   assert.match(metadata.developmentStatus, /not a tagged release/i);
   assert.doesNotMatch(JSON.stringify(metadata), /aggregateRating|reviewRating|customer count/i);
