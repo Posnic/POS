bfc44f9c27094e346aca064973bdfd437e50bc2c
Sridhar Bala <sridharkalaibala@gmail.com>
2026-09-12 12:41:07 +0530

    Merge main into develop: the squash-merged release, so the next release can merge cleanly

--- order/assets/products/script.js
+++ order/assets/products/script.js
@@ -326,10 +326,6 @@
         document.addEventListener("visibilitychange", () => {
             if (document.hidden && listening) listening.stop();
         });
-        sheet.addEventListener("close", () => {
-            openId = "";
-        });
-    })();
 
         mic.addEventListener("click", () => {
             if (listening) {
